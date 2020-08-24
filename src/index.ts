f178d0c45bb0387d050009b07c01fe06728335c6
platane <me@platane.me>
2020-08-24 15:06:26 +0200

    :rocket: tooling

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { add } from "./a";
+
+console.log("coucou");
+
+const myVar = 123;
+
+const b = 12311 + Math.random();
+
+console.log(myVar);
+
+if (add(myVar, b) === 4) {
+  console.log("yolo");
+}
